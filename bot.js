84a72a3395ec59c30f05f7545418588de15d0ccd
SFan042 <32644045+SFan042@users.noreply.github.com>
2018-08-03 19:50:53 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,16 @@
 const Discord = require('discord.js');
+var fs = require('fs');
 var prefix = '!';
 
 const client = new Discord.Client();
 client.on('message', message => {
 	if(message.author === client.user) return;
 	if(message.content.startsWith(prefix + 'тест')) {
-		message.channel.send('ыщы');
-	}
+		var fs = require('fs');
+		fs.readFile('test.txt', 'utf8', function(err,data){
+			message.channel.send(data);
+	});
+}
 });
 
 client.login('Mzk5MDgyNDgxOTU0ODQ4Nzgx.DTH6UQ.498i4tzwCJ5bS2vhWA9d5x5uZWg');
